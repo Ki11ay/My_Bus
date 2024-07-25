07366c011a972842ce99c863590e5ff04a0d94d5
Ki11ay <mohammedaliedriis@gmail.com>
2024-07-26 01:09:25 +0300

    working (have to make mounted for every setstate)

--- lib/Screen/settings.dart
+++ lib/Screen/settings.dart
@@ -3,7 +3,6 @@ import 'package:my_bus/Screen/Languagesc.dart';
 import 'package:my_bus/Screen/helpcentersc.dart';
 import 'package:my_bus/add_data.dart';
 import 'package:my_bus/components/color.dart';
-import 'package:my_bus/home.dart';
 import 'package:my_bus/Screen/splashscreen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
